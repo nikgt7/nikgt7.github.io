de358d30fc39058366e4d62e0937c35a503c765f
nicosdem <nicosdem@yahoo.com>
2017-08-22 15:35:31 +0300

    MagnifyPopup videos, OFTO page updated

--- js/script.js
+++ js/script.js
@@ -112,6 +112,17 @@ app.controller('PageCtrl', function () {
 				}
 			}
 		});
+		
+		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+			disableOn: 700,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+
+			fixedContentPos: false
+		});
+
 	};
 
 	var animateBoxWayPoint = function() {
